957f64530453e9f51fd67e720df84f4a8488ed23
fornesarturo <fornesarturo@gmail.com>
2017-05-27 16:41:25 -0500

    Get Started Payload

--- actions.py
+++ actions.py
@@ -1,8 +1,9 @@
 def generateAnswer(text):
     if "hola" in text.lower():
         return "Hola, gusto en hablar contigo, aunque en realidad esté hardcodeado","text","options"
-    if "get started" in text.lower():
+    if "GET_STARTED_PAYLOAD" in text:
         return "Hola, soy un Bot que procesa tus tramites, para proseguir haz click en algún botón o envía un mensaje","text","welcome"
+    if "PAYLOAD_HELP" in text:
     return "Dame tu INE, anda, confía","text","options"
 
 class EntryManager(object):
@@ -10,7 +11,7 @@ class EntryManager(object):
         self.entry = entry
         self.message_list = []
         for event in entry['messaging']:
-            if(event.get("message")):
+            if(event.get("postback") or event.get("message")):
                 self.message_list.append(event)
     def answerEntry(self):
         def getAnswer(event):
@@ -26,5 +27,9 @@ class EntryManager(object):
                     answer,_type,quick_reply = generateAnswer(text)
                 if "text" in _type:
                     return {'sender':sender,'user_text':text,'text':answer,'type':_type,'quick':quick_reply}
+            if event.get("postback"):
+                payload = event["postback"]["payload"]
+                answer,_type,quick_reply = generateResponse(payload,sender)
+                return {'sender':sender,'user_text':payload,'text':answer,'type':_type,'quick':quick_reply}
         answer_list = map(getAnswer, self.message_list)
         return answer_list
