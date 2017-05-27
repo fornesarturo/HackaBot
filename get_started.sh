6d5448940f21cee210a64dc98b99420e16398d88
fornesarturo <fornesarturo@gmail.com>
2017-05-27 14:03:21 -0500

    Get started

---
+++ get_started.sh
@@ -0,0 +1,5 @@
+curl -X POST -H "Content-Type: application/json" -d '{
+  "get_started":{
+    "payload":"GET_STARTED_PAYLOAD"
+  }
+}' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAEY4sHIr4cBAFxEKpa9QAZBMLdGUUX9ZB5qsaxO8TPDmiVhapqh2a3wLQ2sd4vSUh04eCxWcUZA7EQuEMmEAd4SZB7x8zjaTrX7UeMtoWOGlvKhJ6PPZBZCRZC4jZAOj0QsR0XxKltRZCdvkOJKL5OjJuLvp9LKVThmbcGTm4RLYlAZDZD"
