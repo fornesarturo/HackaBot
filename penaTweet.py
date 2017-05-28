9547e1e04fcaff9b05aafd5137d6d6966d7ededc
fornesarturo <fornesarturo@gmail.com>
2017-05-28 12:48:26 -0500

    Random pena tweet

--- penaTweet.py
+++ penaTweet.py
@@ -1,9 +1,7 @@
+import random
 from twitter import *
 
 def getLastEPNTweet(AT,AT_S,CON,CON_S):
     t = Twitter(auth=OAuth(AT,AT_S,CON,CON_S))
-    tweets = list(t.statuses.user_timeline(screen_name="EPN",count=1))
-
-    #for tweet in tweets:
-        #print(tweet['text'])
-    return tweets[0]['text'] + " - EPN"
+    tweets = list(t.statuses.user_timeline(screen_name="EPN",count=100))
+    return random.choice(tweets)['text'] + " - EPN"
