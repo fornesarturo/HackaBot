f02f860a3608f16efa54d9a652568d68eae8268c
fornesarturo <fornesarturo@gmail.com>
2017-05-28 13:48:51 -0500

    log and data

--- other_functions.py
+++ other_functions.py
@@ -81,8 +81,9 @@ def isThisAnINE(img_url):
     identify_url = "http://gerardo8170.cloudapp.net:8080/identifyIne"
     _data = {"img":img_url}
     log(img_url)
-    r = requests.get(identify_url,json=_data,headers=headers)
+    r = requests.get(identify_url,data=_data,headers=headers)
     if("<!DOCTYPE HTML" in r.text):
+        log("This was an error in request")
         return False
     return eval(r.text)
 
