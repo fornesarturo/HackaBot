8af5ad02bbab339fa4a0017fee43ce87cf4c0279
rhdzmota <rhdzmota@mxquants.com>
2017-05-28 04:46:09 -0500

    change -- again  url

--- other_functions.py
+++ other_functions.py
@@ -80,7 +80,7 @@ def isThisAnINE():
 def sendIne2DB(sender,_url):  
     facebook_data = getUserInfo(sender)
     image_text    = getImageText()    
-    post_url = "http://187.162.211.225:8080/api/ine" 
+    post_url = "http://35.162.169.59:8080/api/ine" 
     ine = {"facebookID":str(sender),
            "fName":"",
            "mName":str(facebook_data.get("last_name")),
@@ -105,7 +105,7 @@ def sendMessage2DB(sender,text,timestamp):
                     "facebookID":str(sender),
                     "timestamp":int(timestamp),
                     "messageText":str(text)}
-    post_url = "http://187.162.211.225:8080/api/message" 
+    post_url = "http://35.162.169.59:8080/api/message" 
     try:
         r = requests.post(post_url,data=message_info)
         print(r.text)
